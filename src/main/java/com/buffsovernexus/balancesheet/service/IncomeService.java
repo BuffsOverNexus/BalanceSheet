b3d71145b4a899c3a0de176cbea42d97e5288c4f
BuffsOverNexus <buffsovernexus2@gmail.com>
2022-12-20 20:28:03 -0500

    Issue #6 - Create an income item

--- src/main/java/com/buffsovernexus/balancesheet/service/IncomeService.java
+++ src/main/java/com/buffsovernexus/balancesheet/service/IncomeService.java
@@ -2,14 +2,14 @@ package com.buffsovernexus.balancesheet.service;
 
 import com.buffsovernexus.balancesheet.entity.AccountEntity;
 import com.buffsovernexus.balancesheet.entity.IncomeEntity;
+import com.buffsovernexus.balancesheet.entity.request.CreateIncomeRequest;
 import com.buffsovernexus.balancesheet.repository.AccountRepository;
 import com.buffsovernexus.balancesheet.repository.IncomeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 public class IncomeService {
@@ -23,6 +23,29 @@ public class IncomeService {
     public ResponseEntity<List<IncomeEntity>> getAllIncomeByAccount(UUID account) {
         AccountEntity accountEntity = accountRepository.getReferenceById(account);
         return ResponseEntity.ok(accountEntity.getIncome());
+    }
+
+    public ResponseEntity<IncomeEntity> createIncomeItem(CreateIncomeRequest createIncomeRequest) {
+        try {
+            AccountEntity accountEntity = accountRepository.getReferenceById(createIncomeRequest.getAccount());
+            // Create income object
+            IncomeEntity incomeEntity = new IncomeEntity();
+            incomeEntity.setAmount(createIncomeRequest.getAmount());
+            incomeEntity.setDescription(createIncomeRequest.getDescription());
+            incomeEntity.setLabel(createIncomeRequest.getLabel());
+            incomeEntity.setReoccurringType(createIncomeRequest.getReoccurringType());
+            incomeRepository.save(incomeEntity);
+            if (Objects.isNull(accountEntity.getIncome())) {
+                accountEntity.setIncome(Collections.singletonList(incomeEntity));
+            } else {
+                accountEntity.getIncome().add(incomeEntity);
+            }
+            accountRepository.save(accountEntity);
+            return ResponseEntity.ok(incomeEntity);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return ResponseEntity.internalServerError().build();
+        }
     }
 
 }
